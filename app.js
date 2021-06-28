58cffa14bbb5cb79b9faae3cf99b4d32baa19523
NadavLalo <nadavlalo@gmail.com>
2021-06-28 13:40:59 +0300

    finished phase3 and added some cleanup

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const readline = require('readline-sync');
+const fs = require('fs');
+const path = './gameHistory.txt';
+
+const getWinningScore = () => {
+  const bestOf = readline.question('Best of how many?');
+  const winningScore = Math.floor(bestOf / 2) + 1;
+
+  return { bestOf, winningScore };
+};
 
 const gameTurn = (playersData, randomNum, round, winningScore) => {
   const turnWinner = randomNum % 2 === 0 ? playersData.p1 : playersData.p2;
@@ -15,10 +24,16 @@ const gameTurn = (playersData, randomNum, round, winningScore) => {
 
   if (turnWinner.score === winningScore) {
     console.log(`${turnWinner.name} Wins!`);
+    fs.appendFileSync(
+      'gameHistory.txt',
+      `${turnWinner.name} Won with a score of ${winningScore} \n`
+    );
+    return turnWinner;
   }
 };
 
 const startGame = () => {
+  console.log('One on One Mode');
   const players = {
     p1: {
       name: readline.question('Enter Player 1 Name: \n'),
@@ -30,18 +45,23 @@ const startGame = () => {
     },
   };
 
-  const bestOf = readline.question('Best of how many?');
-  const winningScore = Math.floor(bestOf / 2) + 1;
+  const winningInfo = getWinningScore();
 
   let round = 1;
 
-  while (players.p1.score < winningScore && players.p2.score < winningScore) {
+  fs.appendFileSync(
+    'gameHistory.txt',
+    `${new Date()}\nPlayers: ${players.p1.name}, ${players.p2.name}\nBest of ${
+      winningInfo.bestOf
+    }\n`
+  );
+
+  while (
+    players.p1.score < winningInfo.winningScore &&
+    players.p2.score < winningInfo.winningScore
+  ) {
     const randomNum = Math.floor(Math.random() * 19) - 5;
-    if (randomNum % 2 === 0) {
-      gameTurn(players, players[0], randomNum, round, winningScore);
-    } else {
-      gameTurn(players, players[1], randomNum, round, winningScore);
-    }
+    gameTurn(players, randomNum, round, winningInfo.winningScore);
     round++;
   }
 };
@@ -58,7 +78,33 @@ const getPlayers = (numOfPlayers = 2) => {
   return players;
 };
 
+const bossFight = player => {
+  console.log(`${player.name} VS Boss`);
+  const players = {
+    p1: player,
+    p2: { name: 'Boss', score: 0 },
+  };
+  let winner;
+  const winningInfo = getWinningScore();
+  fs.appendFileSync(
+    'gameHistory.txt',
+    `${player.name} VS Boss \nBest of ${winningInfo.bestOf}\n`
+  );
+  let round = 1;
+  while (
+    players.p1.score < winningInfo.winningScore &&
+    players.p2.score < winningInfo.winningScore
+  ) {
+    const randomNum = Math.floor(Math.random() * 19) - 5;
+
+    if (randomNum < 0 && randomNum % 2 === 0) continue;
+    winner = gameTurn(players, randomNum, round, winningInfo.winningScore);
+  }
+  console.log(`${winner.name} Wins the game!`);
+};
+
 const startTournemant = () => {
+  console.log('Tournament Mode');
   let numOfPlayers = +readline.question('Enter how many players: \n');
   while (numOfPlayers < 2 || numOfPlayers > 7 || isNaN(numOfPlayers)) {
     numOfPlayers = +readline.question(
@@ -67,12 +113,19 @@ const startTournemant = () => {
   }
 
   const players = getPlayers(numOfPlayers);
-  const bestOf = readline.question('Best of how many?');
-  const winningScore = Math.floor(bestOf / 2) + 1;
+  const winningInfo = getWinningScore();
   let highestScore = 0;
   let round = 1;
 
-  while (highestScore < winningScore) {
+  let winner;
+
+  fs.appendFileSync(
+    'gameHistory.txt',
+    `${new Date()}\nPlayers: ${JSON.stringify(
+      players.map(player => player.name)
+    )} \nBest of ${winningInfo.bestOf}\n`
+  );
+  while (highestScore < winningInfo.winningScore) {
     const randomPlayer1Idx = Math.floor(Math.random() * players.length);
     const randomPlayer2Idx = Math.floor(Math.random() * players.length);
 
@@ -84,7 +137,7 @@ const startTournemant = () => {
     };
 
     const randomNum = Math.floor(Math.random() * 19) - 5;
-    gameTurn(participants, randomNum, round, winningScore);
+    winner = gameTurn(participants, randomNum, round, winningInfo.winningScore);
 
     if (players[randomPlayer1Idx].score > highestScore) {
       highestScore = players[randomPlayer1Idx].score;
@@ -93,8 +146,31 @@ const startTournemant = () => {
     }
     round++;
   }
+
+  bossFight({ ...winner, score: 0 });
 };
 
-startGame();
+const init = () => {
+  if (fs.existsSync(path)) {
+    const fileData = fs.readFileSync(path);
+    console.log('Game History');
+    console.log(fileData.toString('utf-8'));
+  }
 
-// startTournemant();
+  console.log('Choose Game Mode');
+  let userChoice = +readline.question(
+    'Enter 1 for One on One, or 2 for tournament mode: \n'
+  );
+
+  while (userChoice !== 1 && userChoice !== 2) {
+    userChoice = +readline.question('Invalid value. Please try again: \n');
+  }
+
+  if (userChoice === 1) {
+    startGame();
+  } else if (userChoice === 2) {
+    startTournemant();
+  }
+};
+
+init();
