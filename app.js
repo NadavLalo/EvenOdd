d5bd0a4ccb6b985f7590e8bc0b8e10592802179a
NadavLalo <nadavlalo@gmail.com>
2021-06-28 10:55:55 +0300

    Add game function

--- app.js
+++ app.js
@@ -15,3 +15,31 @@ const gameTurn = (playersData, turnWinner, randomNum, round, winningScore) => {
     console.log(`${turnWinner.name} Wins!`);
   }
 };
+
+const gameStart = () => {
+    const players = {
+      p1: {
+        name: readline.question('Enter Player 1 Name: \n'),
+        score: 0,
+      },
+      p2: {
+        name: readline.question('Enter Player 2 Name: \n'),
+        score: 0,
+      },
+    };
+  
+    const bestOf = readline.question('Best of how many?');
+    const winningScore = Math.floor(bestOf / 2) + 1;
+  
+    let round = 1;
+  
+    while (players.p1.score < winningScore && players.p2.score < winningScore) {
+      const randomNum = Math.floor(Math.random() * 19) - 5;
+      if (randomNum % 2 === 0) {
+        gameTurn(players, players.p1, randomNum, round, winningScore);
+      } else {
+        gameTurn(players, players.p2, randomNum, round, winningScore);
+      }
+      round++;
+    }
+  };
